deb48061da7a1ef92134b8d963f1ad440f616eee
V1LL0 <v.cestarelli@gmail.com>
2013-05-15 17:44:55 +0200

    update

--- mosaicImage.js
+++ mosaicImage.js
@@ -16,11 +16,11 @@
 		var imgData = ctx.createImageData(w,h);	
 
 		//blocks coordinates
-		var blocksCoordinates = new Array();
+		//var blocksCoordinates = new Array();
 
 		//Take text area
 		var txArea = document.getElementById("tx_coordinates");
-		txArea.value = "";
+		txArea.value = " T = function (dims) {\n\treturn function (values) {\n\t\treturn function (object) {\n\t\t\treturn object.translate(dims, values);\n\t\t};\n\t};\n}\n\n";
 		
 		for (var y=0; y<h; y+=blockSize) {
 			for (var x=0; x<w; x+=blockSize) {
@@ -71,8 +71,8 @@
 								imgData.data[(y1*4*w)+(x1*4)+1]=0
 								imgData.data[(y1*4*w)+(x1*4)+2]=0
 								imgData.data[(y1*4*w)+(x1*4)+3]=255
-								txArea.value += "DRAW(T([1,2])([" + x1 + "," + y1 + "])(CUBOID([" + blockSizeX +", " + blockSizeY + ", 1])))\n";
-								blocksCoordinates.push([x1,y1]);
+								txArea.value += "DRAW(T([0,1])([" + x1 + "," + y1 + "])(CUBOID([" + blockSizeX +", " + blockSizeY + ", 1])))\n";
+								//blocksCoordinates.push([x1,y1]);
 							}
 						}
 					}
@@ -83,7 +83,7 @@
 
 			}
 		}
-			
+
 		ctx.putImageData(imgData,0,0);
 		console.log(blocksCoordinates[5]);
 		return true;
