d3299e4e5020e9b4bc2ab133473553b467d05a5f
V1LL0 <v.cestarelli@gmail.com>
2013-05-16 15:19:49 +0200

    improved performance with 'horizontal rectangles technique'

--- mosaicImage.js
+++ mosaicImage.js
@@ -18,7 +18,14 @@
 		//Take text area
 		var txArea = document.getElementById("tx_coordinates");
 		txArea.value = " T = function (dims) {\n\treturn function (values) {\n\t\treturn function (object) {\n\t\t\treturn object.translate(dims, values);\n\t\t};\n\t};\n}\n\n";
-		
+
+		//Array of rectangles
+		//A rectangle is an array as [x,y,baseLength,Height]
+		//height is always = blockSizeY
+		//instead baseLength is a multiple of blockSizeX
+		var rectanglesArray = new Array();
+		//
+		var white=true;
 		for (var y=0; y<h; y+=blockSize) {
 			for (var x=0; x<w; x+=blockSize) {
 				
@@ -45,7 +52,12 @@
 					}
 				}
 
-				if(count > (blockSizeX*blockSizeY/2))
+				if(count > (blockSizeX*blockSizeY/2)){
+					if(white===false){
+						rect.push(rectangleLength, blockSizeY);
+						rectanglesArray.push(rect);
+					}
+					white = true;
 					for(var y1=y; y1<y+blockSizeY; y1++){
 						for (var x1=x; x1<x+blockSizeX; x1++){
 							if(y1<h && x1<w){
@@ -56,11 +68,17 @@
 							}
 						}
 					}
-
+				}
 				else{
-					txArea.value += "DRAW(DOMAIN([[" + x + "," + (x+blockSizeX) + "], [" + y + ", " + (y+blockSizeY) + "],[0,2]])([1,1,1]));\n";
+					if(white===true){
+						var rect = new Array();
+						rect.push(x,y);
+						var rectangleLength = 0;
+					}
+					rectangleLength+=blockSizeX;
+					white=false;
 					for(var y1=y; y1<y+blockSizeY; y1++){
-						for (var x1=x; x1<x+blockSizeX; x1++){
+						for (var x1=x; x1<x+rectangleLength*blockSizeX; x1++){
 							if(y1<h && x1<w){
 								imgData.data[(y1*4*w)+(x1*4)]=0
 								imgData.data[(y1*4*w)+(x1*4)+1]=0
@@ -76,6 +94,11 @@
 
 			}
 		}
+
+
+	for(var i=0; i<rectanglesArray.length; i++){
+		txArea.value += "DRAW(DOMAIN([[" + rectanglesArray[i][0] + "," + (rectanglesArray[i][0]+rectanglesArray[i][2]) + "], [" + rectanglesArray[i][1] + ", " + (rectanglesArray[i][1]+rect[3]) + "],[0,2]])([1,1,1]));\n";
+	}
 
 		ctx.putImageData(imgData,0,0);
 		return true;
